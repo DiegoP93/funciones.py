e76ca4476d5dd765b7ee99b317741f57a77c3489
206 <206@academicos.cesde.edu.co>
2022-03-25 20:41:51 -0500

    NTV se crean funciones

---
+++ funcion.py
@@ -0,0 +1,10 @@
+#programa que suma dos numeros
+numero1=int (input("Digite un numero: "))
+numero2=int (input("Digite un numero: "))
+
+print(f"la suma es {numero1+numero2}")
+
+numero3=int (input("Digite un numero: "))
+numero4=int (input("Digite un numero: "))
+
+print(f"la suma es {numero3+numero4}")
---
+++ funcion2.py
@@ -0,0 +1,10 @@
+#funcion
+def sumar(numero1,numero2):
+    suma=numero1+numero2
+    return(suma)
+
+#print(sumar(10,10))
+
+'''resultado=sumar(10,10)
+print(resultado)'''
+
